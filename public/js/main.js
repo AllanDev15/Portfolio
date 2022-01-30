6218b78eae570710f710eb5d0ca8a2ac87998bca
Allan_PC <Allan.FL.15@live.com>
2022-01-29 20:43:28 -0600

    adds the projects section
    Includes:
    - Project preview
    - Project view in a modal window

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
   scrollNavbar();
+
+  // Control the amount of autoscroll in the preview of projects according to the element height
+  const cardPreview = document.querySelector('.project-card__preview');
+  let cardPreviewHeight = cardPreview.offsetHeight;
+  window.addEventListener('resize', () => (cardPreviewHeight = cardPreview.offsetHeight));
+  const translateProperty = `translateY(calc(-100% + ${cardPreviewHeight}px))`;
+  document.querySelector('.projects__content').style.setProperty('--translate', translateProperty);
+
+  // Modal projects
+  const projects = document.querySelector('.projects__content');
+  const modal = document.querySelector('#modal');
+  let modalProject;
+
+  projects.addEventListener('click', (e) => {
+    const selected = e.target;
+    const projectSelected = e.target.dataset.project;
+    if (selected.classList.contains('project-card')) {
+      modal.classList.add('modal--show');
+      modalProject = modal.querySelector(`#${projectSelected}`);
+      modalProject.classList.add('project--show');
+    }
+  });
+
+  modal.addEventListener('click', (e) => {
+    if (e.target.classList.contains('modal')) {
+      e.target.classList.remove('modal--show');
+      modalProject.classList.remove('project--show');
+    }
+
+    if (e.target.classList.contains('close-modal')) {
+      modal.classList.remove('modal--show');
+      modalProject.classList.remove('project--show');
+    }
+  });
 });
 
 function scrollNavbar() {
